54e53d680b38780da6168480c24469c475d3c5a5
tianxiao18 <th2946@columbia.edu>
2025-06-05 20:06:24 -0400

    shape metrics

---
+++ netrep/metrics/__init__.py
@@ -0,0 +1,6 @@
+from netrep.metrics.cka import LinearCKA
+from netrep.metrics.linear import LinearMetric
+from netrep.metrics.perm import PermutationMetric
+from netrep.metrics.stochastic import GaussianStochasticMetric
+from netrep.metrics.stochastic import EnergyStochasticMetric
+from netrep.metrics.stochastic_process import GPStochasticMetric
